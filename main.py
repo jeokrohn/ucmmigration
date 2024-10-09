a2a9623e877b0a3c3c51121be39b4baf7321f91a
Johannes Krohn <johannes.krohn@krohns.de>
2024-10-09 11:14:10 +0200

    - dev dependency: pyinstaller - use getcwd() instead of script directory as base dir to search for TAR files

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     logging.getLogger('digit_analysis.node.traversal').setLevel(logging.INFO)
     logging.getLogger('user_dependency_graph').setLevel(logging.INFO)
     logging.getLogger('ucmexport').setLevel(logging.INFO)
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    cur_dir = os.getcwd()
     tar_files = glob.glob(os.path.join(cur_dir, '*.tar'))
     tar_files = sorted(map(os.path.basename, tar_files))
     assert tar_files, f'No TAR files found in {cur_dir}'
